4dcba0addbd0bd93eb285f8c98ba701bef3ac2e4
leehyun2003 <mogui99@hufs.ac.kr>
2025-09-23 09:09:04 +0900

    feat: hw1-app 계획서

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def home():
     return render_template("home.html", title="Flask 템플릿 연결")
 
+@app.get("/hw1")
+def hw1():
+    return render_template("hw1.html",title="스마트 분리수거 안내")
+
 if __name__=="__main__":
     app.run() # 기본: http://127.0.0.1:500
